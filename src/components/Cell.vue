9e7f9f3b19040096e7f53023d00154b6829a0357
Denis Sipakov <mc.flyer@gmail.com>
2021-03-02 16:43:13 +0300

    Add Cell component

---
+++ src/components/Cell.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="cell">
+        
+    </div>
+</template>
+
+
+<style scoped>
+.cell {
+    position: absolute;
+    width: 10px;
+    height: 10px;
+    top: 100;
+    left: 100;
+    background-color: black;
+}
+</style>
