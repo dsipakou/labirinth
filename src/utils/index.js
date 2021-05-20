7fafced0cd8166260ff99d2e83156d2b8467aebb
Denis Sipakov <mc.flyer@gmail.com>
2021-05-20 21:09:07 +0300

    Mark cells

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,11 +1,18 @@
 export const dijkstra = (maze) => {
   let sizeMatrix = new Array(maze.length).fill(-1).map(() => new Array(maze[0].length).fill(-1));
+  let visitedMatrix = new Array(maze.length).fill(false).map(() => new Array(maze[0].length).fill(false));
   for (let i = 0; i < maze.length; i += 1) {
     for (let j = 0; j < maze[0].length; j += 1) {
       if (maze[i][j] === 0) {
         sizeMatrix[i][j] = Infinity;
+        visitedMatrix[i][j] = true;
       }
     }
+  }
+
+  let stopFlag = false
+  while (!stopFlag) {
+    stopFlag = true;
   }
   console.log(sizeMatrix);
 }
